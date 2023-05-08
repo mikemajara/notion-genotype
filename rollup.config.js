c2b95814a551af05d8b7dc833258b6a96446a8ef
Miguel Alcalde <miguelalcalde@Mikes-MacBook-Pro-16-2019.local>
2023-05-08 23:15:01 +0200

    Add package.json, .gitignore, rollup.config.js, and index.ts files

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+  },
+  plugins: [typescript(), commonjs(), resolve()],
+};
